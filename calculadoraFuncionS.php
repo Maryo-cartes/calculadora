0555d7edbf21a2dc68ccdddb07f48f4bb17d16a4
mayo <mayo.cartes23@gmail.com>
2023-12-16 13:38:08 -0300

    funcion suma

---
+++ calculadoraFuncionS.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+<?php 
+	if ($_POST ["valor1"] !="" and $_POST ["valor2"]!=""){
+		if ($_POST["operador"] == "suma") {
+			print ($resultado = $_POST ["valor1"] + $_POST ["valor2"]);
+			echo "<script>alert('asdf')</script>";
+			print ('<br /><a href="calculadora.php">Volver</a>');
+            }
+            }
+            ?>
